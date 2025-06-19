3b31412390d31cb8e7b43997953d123700eaa91f
Viktor Mrichko <viktor_mrichko@epam.com>
2025-06-19 12:23:49 +0300

    Project creation and basic adjustment
    iOS version, supported devices and supported orientation were chosen.
    Also were created appropriate folders
    .gitignore file were added

---
+++ WeatherApp/WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Viktor Mrichko on 19.06.2025.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
