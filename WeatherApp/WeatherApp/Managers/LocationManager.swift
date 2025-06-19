5cf64e5cd42b7d670cae0b2c6b8aa829fad0e1f5
Viktor Mrichko <viktor_mrichko@epam.com>
2025-06-19 13:20:45 +0300

    Created LocationManager
    Renamed ContentView

---
+++ WeatherApp/WeatherApp/Managers/LocationManager.swift
@@ -0,0 +1,67 @@
+//
+//  LocationManager.swift
+//  WeatherApp
+//
+//  Created by Viktor Mrichko on 19.06.2025.
+//
+
+import CoreLocation
+
+@Observable
+final class LocationManager: NSObject {
+    var cityName = "Defining location..."
+    var errorDescription: String?
+    @ObservationIgnored private lazy var locationManager = CLLocationManager()
+    
+    private func getCityName(from location: CLLocation, completion: @escaping (String?) -> Void) {
+        let geocoder = CLGeocoder()
+        geocoder.reverseGeocodeLocation(location, preferredLocale: .current) { placemarks, error in
+            guard let firstPlacemark = placemarks?.first, error == nil else {
+                self.errorDescription = error?.localizedDescription
+                completion(nil)
+                return
+            }
+            completion(firstPlacemark.locality)
+        }
+    }
+}
+
+extension LocationManager: CLLocationManagerDelegate {
+    func requestCurrentLocation() {
+        locationManager.delegate = self
+    }
+    
+    func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
+        switch locationManager.authorizationStatus {
+        case .restricted, .denied:
+            locationManager.requestWhenInUseAuthorization()
+            errorDescription = "Location services are disabled"
+        case .authorizedAlways, .authorizedWhenInUse:
+            locationManager.startUpdatingLocation()
+        case .notDetermined:
+            locationManager.requestWhenInUseAuthorization()
+        default:
+            return
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        errorDescription = error.localizedDescription
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard let location = locations.first else {
+            cityName = "Undefined"
+            return
+        }
+        
+        getCityName(from: location) { [weak self] cityName in
+            guard let self = self, let cityName = cityName else {
+                self?.errorDescription = "Unable to determine city"
+                return
+            }
+            self.cityName = cityName
+        }
+    }
+}
+
